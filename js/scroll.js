c4d1e246335a8e1227e22c8ce60ab311036c0831
Misael Jordyy <79817432+zhenxxx7@users.noreply.github.com>
2021-11-29 11:48:32 +0700

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,58 @@
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.content', {delay: 650})
+ScrollReveal().reveal('.cf', {delay: 450})
+ScrollReveal().reveal('.heading', {delay: 150})
+ScrollReveal().reveal('.share', {delay: 350})
+ScrollReveal().reveal('.credit', {delay: 450})
+ScrollReveal().reveal('.fs-1', {delay: 350})
+ScrollReveal().reveal('.fs-4', {delay: 550})
+ScrollReveal().reveal('.box-container', {delay: 550})
+
+
+
+ScrollReveal({
+  origin: "left",
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.row .image', {delay: 450})
+ScrollReveal().reveal('.hp', {delay: 450})
+ScrollReveal().reveal('.coffesop', {delay: 450})
+ScrollReveal().reveal('.map', {delay: 450})
+
+
+
+ScrollReveal({
+  origin: "right",
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.str', {delay: 450})
+ScrollReveal().reveal('.float-end', {delay: 450})
+ScrollReveal().reveal('.bttn', {delay: 750})
+
+
+
+ScrollReveal({
+  origin: "top",
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.addr', {delay: 450})
+ScrollReveal().reveal('.h3-loc', {delay: 650})
+ScrollReveal().reveal('.inputBox', {delay: 850})
